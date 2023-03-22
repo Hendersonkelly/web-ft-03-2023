f466b7eb2c1dac9a5c8aa5dfba746b2116381996
Kelly Henderson <kellyjeanhenderson@gmail.com>
2023-03-22 14:55:31 -0500

    hw

---
+++ lectures/week02/3-Wednesday/MY-LectureNotes/hw-js101-dataTypes/homework.js
@@ -0,0 +1,70 @@
+
+
+//Problem 1
+// let firstName = "Kelly "
+// let lastName = "Henderson"
+// let name = firstName + lastName
+
+// console.log(name);
+
+
+//Problem 2
+//  let a = 2
+//  let b = 3
+//  let x = a + b
+//  console.log(x);
+
+
+ //problem 3
+// let x = 3
+// let y = 15
+// let result = x + y
+// console.log(result);
+
+// let x = "3"
+// let y = "15"
+// let x_num = Number(x)
+// let y_num = Number(y)
+// let result = x_num + y_num
+// console.log(result);
+
+// Problem 4
+
+// let num1 = 3
+// let num2 = 5
+// let product = num1 * num2
+// console.log(`The product of ${num1} and ${num2} is ${product}`)
+
+// Problem 5
+// let num = 2
+// let square = Math.pow(2, 2)
+// console.log(`The square of ${num} is ${square}`);
+
+//Problem 6
+// num1 = 5
+// num2 = 1
+// let difference = num1 - num2
+// console.log(`The difference between ${num1} and ${num2} is ${difference}`);
+
+//Problem 7
+// let num1 = 12
+// let num2 = 4
+// let quotient = num1 / num2
+// console.log(`The quotient of ${num1} and ${num2} is ${quotient}`);
+
+//Problem 8
+// let a = -234
+// let b = -89
+// let c = -234 + -89
+
+// let result = Math.abs(c)
+// console.log(result);
+
+// Problem 9
+// let result = Math.pow(6, 7)
+// console.log(result);
+
+// problem 10
+// const pi = 3.14
+// let result = Math.round(pi)
+// console.log(result);
